a8e19d53cd62fdd45afeb390611e3d8344838c71
HHang <hienhang@hollandandbarrett.com>
2024-03-31 02:08:45 +0100

    added shadcn, css variables

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,6 +1,11 @@
+import { Button } from '@/components/ui/button';
 
 export default async function create() {
-  const testing = "testing"
-
-  return <> from create</>;
+  // redirct to home page
+  return (
+    <>
+      <h1> Welcome to send me a song</h1>
+      <Button>hellp</Button>
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import { SpotifyApi } from '@spotify/web-api-ts-sdk';
-
 export default async function Home() {
   return <>Hello welcome to Send Me A Song</>;
 }
