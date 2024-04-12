6569550c165dcecd7ab3af4593884490cff9be40
HHang <hienhang@hollandandbarrett.com>
2024-04-12 21:59:05 +0100

    added prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <>Hello welcome to Send Me A Song</>;
+  return (
+    <>
+      <h1 className='title'>Send Me A Song</h1>
+      <p className='subtite'>Discover new music</p>
+    </>
+  );
 }
